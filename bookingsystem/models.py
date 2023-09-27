6594b3359f95e5cf3ba37d58b3b62ebb9c48591a
Melhefnawi <>
2023-09-27 19:58:24 +0000

    creating the model

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Booking_details(models.Model):
+    Date = models.DateField()
+    Time = models.TimeField()
+    People = models.IntegerField()
+    First_Name = models.CharField(max_length=200)
+    Last_Name = models.CharField(max_length=200)
+    Email = models.EmailField()
+    Phone = models.IntegerField()
+    Comment = models.TextField()
+
