35e10277ce710920ca9224d8b414c805de849e56
Melhefnawi <>
2023-10-07 09:15:24 +0000

    builing the tempate for project

--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Booking_details, Client, Menu
 # Create your views here.
+
+
+class BookingList(generic.ListView):
+
+    model = Booking_details
+    queryset = Booking_details.objects.all()
+    template_name = 'booking/index.html'
+
+
