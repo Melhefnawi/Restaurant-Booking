331e3c91c7571212fc2853df2984ea8a63d260eb
Melhefnawi <>
2023-10-07 10:21:32 +0000

    adding Booking_details class

--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic, view
 from .models import Booking_details, Client, Menu
 # Create your views here.
 
@@ -10,4 +10,11 @@ class BookingList(generic.ListView):
     queryset = Booking_details.objects.all()
     template_name = 'booking/index.html'
 
+
+class BookingDetails(view):
+
+    def get(self, request, First_Name, *args, **kwargs):
+
+        queryset = Booking_details.objects.all()
+        booking = get_object_or_404(queryset, First_Name=First_Name)
 
