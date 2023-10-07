b08387ca1db391cba86a323e7d1f5b8d7d476bec
Melhefnawi <>
2023-10-07 13:46:55 +0000

    building the HTML pages

--- bookingsystem/urls.py
+++ bookingsystem/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.BookingList.as_view(), name='home')
+    path('', views.BookingList.as_view(), name='home'),
+    path('<str:First_Name>', views.BookingDetails.as_view(),
+         name='Booking_details')
 ]
--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views import generic, view
+from django.views import generic, View
 from .models import Booking_details, Client, Menu
 # Create your views here.
 
@@ -11,10 +11,12 @@ class BookingList(generic.ListView):
     template_name = 'booking/index.html'
 
 
-class BookingDetails(view):
+class BookingDetails(View):
 
     def get(self, request, First_Name, *args, **kwargs):
 
         queryset = Booking_details.objects.all()
         booking = get_object_or_404(queryset, First_Name=First_Name)
+        
+        return render(request, "booking/booking_details.html", {"booking": booking})
 
