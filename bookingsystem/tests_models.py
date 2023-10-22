4b9aeaccdea0baa35280a341ba53816434a348cd
Melhefnawi <>
2023-10-22 17:09:50 +0000

    adding tests to views

--- bookingsystem/tests_models.py
+++ bookingsystem/tests_models.py
@@ -10,6 +10,9 @@ class TestModels(TestCase):
     def test_done_defaults_to_false(self):
 
         booking_test = Booking_details.objects.create(First_Name='MO', Last_Name='MO',
-                                                      Date='2023-3-4', Time='4:00', 
+                                                      Date='2023-3-4', Time='4:00',
                                                       Email='mmm@yyy.com', Phone_Number=98888, Slug='998877', People_No=6)
+        booking_test_2 = Menu.objects.create(
+            Featured_image='placeholder', Meal_Name='beef burger', Meal_des='beef burger')
+
         self.assertFalse(booking_test.approved)
