91330ca23e225a0589b265b10d623e7b7c301c2e
Melhefnawi <>
2023-12-07 20:21:13 +0000

    disable the previous date in the form

--- bookingsystem/forms.py
+++ bookingsystem/forms.py
@@ -1,11 +1,19 @@
 from .models import Booking_details
 from django import forms
 from crispy_forms.helper import FormHelper
+from django.utils.timezone import now
 from crispy_forms.layout import Submit, Layout, Field
 from crispy_forms.bootstrap import (
     PrependedText, PrependedAppendedText, FormActions)
 
 # Creating the Booking_Form 
+
+class FutureDateInput(forms.DateInput):
+    input_type = 'date'
+
+    def get_context(self, name, value, attrs):
+        attrs.setdefault('min', now().strftime('%Y-%m-%d'))
+        return super().get_context(name, value, attrs)
 
 
 class BookingForms(forms.ModelForm):
@@ -16,20 +24,20 @@ class BookingForms(forms.ModelForm):
         fields = ('First_Name', 'Last_Name', 'Date',
                   'Time', 'Phone_Number', 'People_No', 'Email', )
         widgets = {
-            'Date': forms.DateInput(
-                format=('%Y-%m-%d'),
+            'Date' : FutureDateInput(),
+            'Time': forms.DateInput(
+                format=("%H:%M"),
                 attrs={'class': 'form-control', 
                        'placeholder': 'Select a date',
-                       'type': 'Date',  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
-                      }),
-            'Time': forms.TimeInput(
-                format=('%H:%M'),
-                attrs={'class': 'form-control', 
-                       'placeholder': 'Select a time',
                        'type': 'time'  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
                       }),
+                
         }        
 
 
 
+
+
+
+            
        
