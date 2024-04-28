b6c749932d9a44a2af16139157b668612e5c1cd9
Melhefnawi elhefnawy <mmm_hefnawy@yahoo.com>
2024-04-28 13:28:11 +0000

    modify the booking card

--- arabian/settings.py
+++ arabian/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['8000-melhefnawi-restaurantbo-1ivcdeni2rn.ws-eu110.gitpod.io',
+ALLOWED_HOSTS = ['8000-melhefnawi-restaurantbo-jiylf7r8i2g.ws-eu110.gitpod.io',
                  'arabian.herokuapp.com', 'localhost',
                  'arabian-8c3cc574e38b.herokuapp.com']
 
